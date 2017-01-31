c9b80a057394f9b5d4905daadaeeef397cdce12c
rscherl17 <robertscherl@wireless-s0-no-150-6-34.uchicago.edu>
2017-01-31 00:09:00 -0600

    submission

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,23 @@
 class GamesController < ApplicationController
   def play_rock
 
+    @hand_user = "fa-hand-rock-o"
+    @user_move = "Rock"
+
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Rock"
+      @hand = "fa-hand-rock-o"
       @outcome = "tied"
+      @label = "label-warning"
     elsif @computer_move == "Paper"
+      @hand = "fa-hand-paper-o"
       @outcome = "lost"
+      @label = "label-danger"
     else
+      @hand = "fa-hand-scissors-o"
       @outcome = "won"
+      @label = "label-success"
     end
 
     render("games/play_rock.html.erb")
@@ -16,14 +25,23 @@ class GamesController < ApplicationController
 
   def play_paper
 
+    @hand_user = "fa-hand-paper-o"
+    @user_move = "Paper"
+
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Paper"
+      @hand = "fa-hand-paper-o"
       @outcome = "tied"
+      @label = "label-warning"
     elsif @computer_move == "Scissors"
+      @hand = "fa-hand-scissors-o"
       @outcome = "lost"
+      @label = "label-danger"
     else
+      @hand = "fa-hand-rock-o"
       @outcome = "won"
+      @label = "label-success"
     end
 
     render("games/play_paper.html.erb")
@@ -31,14 +49,23 @@ class GamesController < ApplicationController
 
   def play_scissors
 
+    @hand_user = "fa-hand-scissors-o"
+    @user_move = "Scissors"
+
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Scissors"
+      @hand = "fa-hand-scissors-o"
       @outcome = "tied"
+      @label = "label-warning"
     elsif @computer_move == "Rock"
+      @hand = "fa-hand-rock-o"
       @outcome = "lost"
+      @label = "label-danger"
     else
+      @hand = "fa-hand-paper-o"
       @outcome = "won"
+      @label = "label-success"
     end
 
     render("games/play_scissors.html.erb")
